6ccef2ea910258fd74c1a5f3b38d4885e05ed1aa
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-24 17:14:49 +0500

    hero-banner

---
+++ src/app/components/hero-banner/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import { callToAction, description, greeting, intro, navbarData } from '../../shared/LinkData';
+
+const Hero = () => {
+  return (
+    <section className='flex bg-black h-1/4 text-white px-8 pt-8 justify-evenly items-center'>
+      <div className='w-1/2'>
+        <div className='mb-4'>
+          <p><span className='text-7xl'><span className='text-gray-500'>{greeting}&nbsp;</span>{intro}</span></p>
+        </div>
+        <div className='text-center w-3/4'>
+          <p>{description}</p>
+          <div className='text-center mt-8'>
+            <p className='border border-white border-opacity-30 hover:border-opacity-70 transition'><Link className='underline-offset-4' href={navbarData[1].href}>{callToAction}</Link></p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className='text-white'>
+          <Image src={'/images/hero-banner.png'} width={500} height={500} alt='hero-banner' />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
