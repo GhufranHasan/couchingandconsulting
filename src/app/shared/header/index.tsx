7f1bb8d91fbc91684486b9cc3cf875d110fe1f71
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-24 17:17:50 +0500

    Header

--- src/app/shared/header/index.tsx
+++ src/app/shared/header/index.tsx
@@ -1,13 +1,17 @@
 "use client";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { logoImageLink, logoText, navbarData } from '../LinkData';
+import './header.css';
 
-// components/Header.tsx
-const Header = () => {
+const Header: React.FC = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
-    const handleOutsideClick = () => {
-      setShowDropdown(false);
+    const handleOutsideClick = (event: any) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setShowDropdown(false);
+      }
     };
 
     if (showDropdown) {
@@ -19,54 +23,57 @@ const Header = () => {
     };
   }, [showDropdown]);
 
-  const toggleDropdown = () => {
+  const toggleDropdown = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
     setShowDropdown(!showDropdown);
   };
-    return (
-      <nav className="bg-black text-white">
-        <div className="flex items-center justify-between py-4 px-8">
-          {/* Logo */}
-          <div className="flex items-center">
-            <img src="logo.png" alt="Logo" className="w-32 mr-2" />
-            <span className="text-xl font-semibold">HAROON RANA</span>
-          </div>
-  
-          {/* Menu Items */}
-          <ul className="flex space-x-6">
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li className="relative">
-              <a href="#services" onClick={toggleDropdown}>Services</a>
-              {/* Dropdown Menu */}
-            <ul className={`absolute top-10 left-0 bg-black text-white py-2 px-4 space-y-2 ${
-                showDropdown ? 'block' : 'hidden'
-              }`}>
-              <li className="hover:bg-white hover:text-black">
-                <a href="#coaching">Coaching</a>
-              </li>
-              <li className="hover:bg-white hover:text-black">
-                <a href="#consulting">Consulting</a>
-              </li>
-              <li className="hover:bg-white hover:text-black">
-                <a href="#public-speaking">Public Speaking</a>
-              </li>
-              <li className="hover:bg-white hover:text-black">
-                <a href="#state-of-city">State of City</a>
-              </li>
-            </ul>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
+
+  return (
+    <nav className="bg-black text-white">
+      <div className="flex items-center justify-between py-4 px-16">
+        {/* Logo */}
+        <div className="flex items-center">
+          <img src={logoImageLink} alt="Logo" className="w-10 h-10 mr-2" />
+          <span className="text-xl font-semibold">{logoText.toUpperCase()}</span>
         </div>
-      </nav>
-    );
-  };
-  
-  export default Header;
-  
+
+        {/* Menu Items */}
+        <ul className="flex space-x-6 py-2 font-medium">
+          {navbarData.map((item, index) => (
+            <li
+              key={index}
+              className={`py-2 pl-3 pr-4 text-${index === 0 ? 'white' : 'gray-500'} rounded ${
+                index === 2 ? 'relative hidden md:block' : ''
+              } hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-50 md:p-0`}
+            >
+              <a
+                href={item.href}
+                onClick={index === 2 ? toggleDropdown : undefined}
+                className={index === 2 ? 'cursor-pointer' : ''}
+              >
+                {item.label}
+              </a>
+              {/* Submenu */}
+              {index === 2 && (
+                <ul
+                  ref={dropdownRef}
+                  className={`absolute top-10 left-0 bg-black text-gray-500 py-2 px-4 space-y-2 ${
+                    showDropdown ? 'block' : 'hidden'
+                  } dropdown-menu`}
+                >
+                  {item.subItems?.map((subItem, subIndex) => (
+                    <li key={subIndex} className="hover:text-white">
+                      <a href={subItem.href}>{subItem.label}</a>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
