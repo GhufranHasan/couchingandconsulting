6ab0b4764f23fe29406ca831e047d13c818d36cc
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-24 17:10:59 +0500

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "./components/hero"
+import HomePage from "./components/home";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-24">
-      <Hero />
+    <main className="flex min-h-screen flex-col">
+      <HomePage />
     </main>
   )
 }
