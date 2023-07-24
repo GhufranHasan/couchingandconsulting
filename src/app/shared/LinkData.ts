7777c8730338b5854433a57501da4e3b17f13f79
Ghufran Hasan <ghufran.aims@gmail.com>
2023-07-24 17:17:01 +0500

    Data file - shared

---
+++ src/app/shared/LinkData.ts
@@ -0,0 +1,37 @@
+// Header & Footer
+export const logoText = "Haroon Rana";
+export const logoImageLink = "logo.png"; 
+export const navbarData = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About Me",
+    href: "/about",
+  },
+  {
+    label: "My course",
+    href: "/my-course",
+    subItems: [
+      { label: "Coaching", href: "/coaching" },
+      { label: "Consulting Products", href: "#consultingProducts" },
+      { label: "Public Speaking", href: "#public-speaking" },
+      { label: "State of Subsidy", href: "#state-of-subsidy" },
+    ],
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];
+export const address = "Gulyana Road Kharian"
+
+// Hero Banner
+export const greeting = "Hello!";
+export const intro = "I'm Haroon";
+export const description = "A tenacious, passionate marketer and maker who loves turning bold ideas into impactful & memorable experiences.";
+export const callToAction = "Curious to know more!";
+
+// Footer
+export const heading_title = "Useful Links";
